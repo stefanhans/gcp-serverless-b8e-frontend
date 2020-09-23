6bee1805bf30c4de6080209c64b8e8783f703d96
“stefanhans” <uni@stefan-hans.de>
2020-09-23 13:40:04 +0200

    finished raw version of master data functionality for vehicles - next is implementing backend functionality for master data

--- frontend/lib/feature_modules/landing_page/util/net.dart
+++ frontend/lib/feature_modules/landing_page/util/net.dart
@@ -19,7 +19,7 @@ Future<List<Vehicle>> createMasterDataResponse() async {
         'https://europe-west3-serverless-devops-play.cloudfunctions.net/get-vehicles';
   }
 
-  print("url: " + url);
+  print("net.dart url: " + url);
 
   final http.Response response = await http.post(
     url,
@@ -29,172 +29,17 @@ Future<List<Vehicle>> createMasterDataResponse() async {
     body: jsonEncode(<String, String>{}),
   );
 
-  print("body: " + response.body);
-  print(json.decode(response.body));
+  print("net.dart body: " + response.body);
 
+  List<Vehicle> vehicles = MasterData.fromJson(json.decode(response.body)).vehicles;
 
-
-  List<Vehicle> lv = new List<Vehicle>();
-
-  for ( var vehicle in MasterDataResponse.fromJson(json.decode(response.body)).MasterData) {
-
-
-    print("vehicle: " + vehicle.toString());
-  }
-
-  Map<String, dynamic> md =
-      MasterDataResponse.fromJson(json.decode(response.body)).MasterData[0];
-
-  print("md[GeoPoint]: "+md["GeoPoint"].toString());
-
-//  Map<String, dynamic> mapGp = json.decode(md["GeoPoint"]);
-//
-//  print("mapGp[Latitude]: "+mapGp["Latitude"]);
-
-//  Geopoint gp = Geopoint(Latitude: 0.1, Longitude: 0.2);
-
-//  print(json.decode(md["GeoPoint"].toString()));
-
-//  Map<String, double> mgp = Geopoint.fromJson(json.decode(md["GeoPoint"].toString()));
-  print(" XXXXX { \"latitude\": 66, \"longitude\": 55}");
-
-//  Geopoint gp = Geopoint.fromJson(json.decode("{ \"latitude\": 66, \"longitude\": 55}"));
-
-  print(md["GeoPoint"].toString().replaceAll("latitude", "\"latitude\'").replaceAll("longitude", "\"longitude\""));
-  Geopoint gp = Geopoint.fromJson(json.decode(md["GeoPoint"].toString().replaceAll("latitude", "\"latitude\"").replaceAll("longitude", "\"longitude\"")));
-  lv.add(Vehicle(
-      DocId: md["DocId"],
-      Name: md["Name"],
-      Type: md["Type"],
-      Status: md["Status"],
-      ParkingLot: md["ParkingLot"],
-      GeoPoint: gp,
-      Description: md["Description"]));
+  print("net.dart vehicles.length: " + vehicles.length.toString());
+  print("net.dart vehicles.toString: " + vehicles.toString());
+  print("net.dart vehicles[0].toJson(): " + vehicles[0].toJson().toString());
 
   if (response.statusCode == 200) {
-    return lv;
+    return vehicles;
   } else {
-    return lv;
-    //Vehicles.fromJson(json.decode(response.body))
+    return vehicles;
   }
 }
-
-// Future<String> createMasterDataResponse(
-//     {String text = "",
-//       String sourceLanguage = 'en',
-//       String targetLanguage = 'en'}) async {
-//   if (kIsWeb) {
-//     url =
-//     'https://cors-anywhere.herokuapp.com/ https://europe-west3-serverless-devops-play.cloudfunctions.net';
-//   } else {
-//     url =
-//     ' https://europe-west3-serverless-devops-play.cloudfunctions.net';
-//   }
-
-//   print("text: " + text);
-
-//   final http.Response response = await http.post(
-//     url,
-//     headers: <String, String>{
-//       'Content-Type': 'application/json; charset=UTF-8',
-//     },
-//     body: jsonEncode(<String, String>{
-//       'clientVersion': "0.0.1",
-//       'clientId': "beab10c6-deee-4843-9757-719566214526",
-//       'text': text,
-//       'sourceLanguage': sourceLanguage,
-//       'targetLanguage': targetLanguage,
-//     }),
-//   );
-//   if (response.statusCode == 200) {
-// //    translationDisplay.status = "translated";
-//     return TranslationResponse.fromJson(json.decode(response.body));
-//   } else {
-//     return TranslationResponse.error(response.body.toString());
-//   }
-// }
-
-// class TranslationDisplay {
-//   String taskId;
-//   String sourceText;
-//   String sourceLanguage;
-//   String targetText;
-//   String targetLanguage;
-//   String status;
-
-//   TranslationDisplay(String taskId, String sourceText, String sourceLanguage,
-//       String targetText, String targetLanguage, String status) {
-//     this.sourceText = sourceText;
-//     this.sourceLanguage = sourceLanguage;
-//     this.targetText = targetText;
-//     this.targetLanguage = targetLanguage;
-//     this.status = status;
-//   }
-// }
-
-// class Translator {
-//   String clientVersion;
-//   String clientId;
-//   String text;
-//   String sourceLanguage;
-//   String targetLanguage;
-
-//   Translator(String clientVersion, String clientId, String text,
-//       String sourceLanguage, String targetLanguage) {
-//     this.clientVersion = clientVersion;
-//     this.clientId = clientId;
-//     this.text = text;
-//     this.sourceLanguage = sourceLanguage;
-//     this.targetLanguage = targetLanguage;
-//   }
-
-//   String toJson() {
-//     return ('{ \n"clientVersion": ${this.clientVersion}, '
-//         '\n"clientId": ${this.clientId}, '
-//         '\n"text": ${this.text}, '
-//         '\n"sourceLanguage": ${this.sourceLanguage}, '
-//         '\n"targetLanguage": ${this.targetLanguage}, '
-//         '\n}');
-//   }
-// }
-
-// class TranslationResponse {
-//   final String taskId;
-//   final String translatedText;
-//   final List<String> loadCommands;
-
-//   TranslationResponse({this.taskId, this.translatedText, this.loadCommands});
-
-//   factory TranslationResponse.fromJson(Map<String, dynamic> json) {
-//     print("factory TranslationResponse");
-//     print(json.toString());
-
-//     print("####");
-//     print(translator.toJson());
-//     print("####");
-
-// //    translationDisplay.taskId = json['taskId'];
-// //    translationDisplay.sourceLanguage = translator.sourceLanguage;
-// //    translationDisplay.sourceText = translator.text;
-// //    translationDisplay.targetLanguage = translator.targetLanguage;
-// //    translationDisplay.targetText = json['translatedText'];
-
-//     translator.sourceLanguage = translator.targetLanguage;
-
-//     return TranslationResponse(
-//       taskId: json['taskId'],
-//       translatedText: json['translatedText'],
-//       loadCommands: json['loadCommands'].cast<String>(),
-//     );
-//   }
-
-//   factory TranslationResponse.error(String description) {
-//     print("factory TranslationResponse error");
-//     print(description);
-//     return TranslationResponse(
-//       taskId: "error",
-//       translatedText: description,
-//       loadCommands: null,
-//     );
-//   }
-// }
--- frontend/lib/feature_modules/landing_page/util/types.dart
+++ frontend/lib/feature_modules/landing_page/util/types.dart
@@ -1,33 +1,27 @@
-class Geopoint {
-  double Latitude;
-  double Longitude;
+import 'dart:convert';
 
-  Geopoint({double Latitude, double Longitude}) {
-    this.Latitude = Latitude;
-    this.Longitude = Longitude;
-  }
+class MasterData {
+  List<Vehicle> vehicles;
 
-  factory Geopoint.fromJson(Map<String, dynamic> json) {
-    print("types.dart Geopoint:");
-    print(json.toString());
+  MasterData({this.vehicles});
 
-    return Geopoint(
-      Latitude: double.parse(json["latitude"].toString()),
-      Longitude: double.parse(json["longitude"].toString())
+  factory MasterData.fromJson(List<dynamic> jsonList) {
+    print("types.dart MasterData.fromJson:");
+
+    List<Vehicle> _vehicles = new List<Vehicle>();
+    for (var vehicle in jsonList) {
+      _vehicles.add(Vehicle.fromJson(vehicle));
+    }
+
+    return MasterData(
+      vehicles: _vehicles,
     );
   }
 
-  Map<String, dynamic> toJson() =>
-      {
-        'latitude': Latitude,
-        'longitude': Longitude,
-      };
 
-  String toString() {
-    return "Geopoint:\n"
-        +"\tlatitude: "+this.Latitude.toString()+"\n"
-        +"\tlongitude: "+this.Longitude.toString()+"\n";
-  }
+  Map<String, dynamic> toJson() => {
+    'vehicles': vehicles,
+  };
 }
 
 class Vehicle {
@@ -56,102 +50,89 @@ class Vehicle {
     this.Description = Description;
   }
 
-  String toString() {
-    return "Vehicle:\n"
-        +"DocId: "+this.DocId+"\n"
-        +"Name: "+this.Name+"\n"
-        +"Type: "+this.Type+"\n"
-        +"Status: "+this.Status+"\n"
-        +"ParkingLot: "+this.ParkingLot+"\n"
-        +this.GeoPoint.toString()
-        +"Description: "+this.Description+"\n";
-  }
+  factory Vehicle.fromJson(Map<String, dynamic> jsonMap) {
+    print("types.dart Vehicle.fromJson:");
+    print(jsonMap.toString());
 
-  //
-//  factory Vehicle.fromJson(Map<String, dynamic> json) {
-//    print("factory Vehicles");
-//    print(json.toString());
-//
-//    Geopoint gp;
-//
-//    gp.Latitude = 0.1;
-//    gp.Longitude = 0.2;
-//
-//    Vehicle v = Vehicle(
-//        DocId: "a",
-//        Name: "a",
-//        Type: "a",
-//        Status: "a",
-//        ParkingLot: "",
-//        GeoPoint: gp,
-//        Description: "");
-//
-//    List<Vehicle> lv;
-//    lv.add(v);
-//
-//
-//
-//    return v;
-//  }
-}
-
-
-
-class MasterDataResponse {
-//  final List<Vehicles>{};
-  final List<dynamic> MasterData;
-
-  MasterDataResponse({this.MasterData});
-
-  factory MasterDataResponse.fromJson(List<dynamic> json) {
-    print("types.dart MasterDataResponse:");
-    print(json.toString());
-
-    return MasterDataResponse(
-      MasterData: json,
+    return Vehicle(
+      DocId: jsonMap["DocId"],
+      Name: jsonMap["Name"],
+      Type: jsonMap["Type"],
+      Status: jsonMap["Status"],
+      ParkingLot: jsonMap["ParkingLot"],
+      GeoPoint: Geopoint.fromJson(json.decode(jsonMap["GeoPoint"]
+          .toString()
+          .replaceAll("latitude", "\"latitude\"")
+          .replaceAll("longitude", "\"longitude\""))),
+      Description: jsonMap["Description"],
     );
   }
+
+
+  Map<String, dynamic> toJson() => {
+    'DocId': DocId,
+    'Name': Name,
+    'Type': Type,
+    'Status': Status,
+    'ParkingLot': ParkingLot,
+    'GeoPoint': GeoPoint,
+    'Description': Description,
+  };
+
+  String toString() {
+    return "Vehicle:\n" +
+        "DocId: " +
+        this.DocId +
+        "\n" +
+        "Name: " +
+        this.Name +
+        "\n" +
+        "Type: " +
+        this.Type +
+        "\n" +
+        "Status: " +
+        this.Status +
+        "\n" +
+        "ParkingLot: " +
+        this.ParkingLot +
+        "\n" +
+        this.GeoPoint.toString() +
+        "Description: " +
+        this.Description +
+        "\n";
+  }
 }
 
-//factory TranslationResponse.fromJson(Map<String, dynamic> json) {
-//print("factory TranslationResponse");
-//print(json.toString());
-//
-//print("####");
-//print(translator.toJson());
-//print("####");
-//
-//translationDisplay.taskId = json['taskId'];
-//translationDisplay.sourceLanguage = translator.sourceLanguage;
-//translationDisplay.sourceText = translator.text;
-//translationDisplay.targetLanguage = translator.targetLanguage;
-//translationDisplay.targetText = json['translatedText'];
-//
-//translator.sourceLanguage = translator.targetLanguage;
-//
-//return TranslationResponse(
-//taskId: json['taskId'],
-//translatedText: json['translatedText'],
-//loadCommands: json['loadCommands'].cast<String>(),
-//);
-//
-//
-//
-//
-//factory TranslationResponse.error(String description) {
-//print("factory TranslationResponse error");
-//print(description);
-//return TranslationResponse(
-//taskId: "error",
-//translatedText: description,
-//loadCommands: null,
-//);
-//}
+class Geopoint {
+  double Latitude;
+  double Longitude;
 
-//DocId       string         `firestore:"doc-id"`
-//Name        string         `firestore:"name"`
-//Type        string         `firestore:"type"`
-//Status      string         `firestore:"status"`
-//ParkingLot  string         `firestore:"parking-lot"`
-//GeoPoint    *latlng.LatLng `firestore:"geopoint"`
-//Description string         `firestore:"description"`
+  Geopoint({double Latitude, double Longitude}) {
+    this.Latitude = Latitude;
+    this.Longitude = Longitude;
+  }
+
+  factory Geopoint.fromJson(Map<String, dynamic> json) {
+    print("types.dart Geopoint:");
+    print(json.toString());
+
+    return Geopoint(
+        Latitude: double.parse(json["latitude"].toString()),
+        Longitude: double.parse(json["longitude"].toString()));
+  }
+
+  Map<String, dynamic> toJson() => {
+        'latitude': Latitude,
+        'longitude': Longitude,
+      };
+
+  String toString() {
+    return "Geopoint:\n" +
+        "\tlatitude: " +
+        this.Latitude.toString() +
+        "\n" +
+        "\tlongitude: " +
+        this.Longitude.toString() +
+        "\n";
+  }
+}
